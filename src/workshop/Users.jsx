0569645e30de25869b8341b664487923e4b0e427
tonlakla2548 <ton_lakla2548@hotmail.com>
2022-04-28 16:38:36 +0700

    do useEffect by event click button

--- src/workshop/Users.jsx
+++ src/workshop/Users.jsx
@@ -1,15 +1,24 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Users = () => {
-const [users, setUsers] = useState([]);
-   axios.get('https://jsonplaceholder.typicode.com/users')
-   .then(function (response){
-     console.log(response);
-   }
-   )
+  const [users, setUsers] = useState([]);
+  const [counter, setCounter] = useState(1);
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users/'+counter)
+      .then(function (response) {
+        setUsers(response.data);
+      }
+      )
+  }, [counter]);
+
+  console.log(users)
   return (
-    <div>Demo with API
+    <div>
+        Demo with API
+        <button onClick={()=> setCounter(counter<10?counter+1:1)}>Add</button>
+        <h2>{users.name}</h2>
+        {/* {users.map(user => <li key={user.id}>{user.name}</li>)} */}
     </div>
   )
 }
